56110fd981e76c51442ccb9c059ba9adce238b61
asterite <ary@esperanto.org.ar>
2011-03-18 10:59:06 -0300

    More seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
+# coding: utf-8
+
 Food.delete_all
 
 {"HAMBURGUESAS" =>
    {"_" => [
            ["1- Sola", "10.00"],
            ["2- Con queso o tomate", "12.00"],
+           ["3- Con queso y tomate o jamón", "14.00"],
+           ["3- Bis Con queso, huevo y panceta", "17.00"],
+           ["4- Con queso, cebolla, tomate y lechuga", "16.00"],
+           ["5- Con jamón, queso, tomate y lechuga", "17.00"],
+           ["6- Con jamón, queso, tomate, huevo y lechuga", "18.00"],
    ]
    }
 }.each do |category, subcategories|
