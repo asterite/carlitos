a0a2806e31679cac5605b7e85388953a2dc0f416
asterite <ary@esperanto.org.ar>
2011-03-18 22:55:08 -0300

    Done second page

--- db/seeds.rb
+++ db/seeds.rb
@@ -47,6 +47,54 @@ Food.delete_all
            ["AROS DE CEBOLLA 1 PORCIÓN", "15.00"],
            ["AROS DE CEBOLLA 1/2 PORCIÓN", "12.00"],
      ]
+   },
+ "LOMITOS" => {
+     "" => [
+       ["32- Solo", "19.00"],
+       ["33- Con queso o tomate", "21.00"],
+       ["34- Con queso, tomate o jamón", "22.00"],
+       ["35- Con queso, cebolla, tomate y lechuga", "23.00"],
+       ["36- Con jamón, queso, tomate y lechuga", "24.00"],
+       ["37- Con jamón, queso, tomate huevo y lechuga", "25.00"],
+       ["38- Completo sin huevo, con panceta o jamón", "26.00"],
+       ["39- Completo con huevo, con panceta o jamón", "28.00"],
+       ["40- Completo sin huevo, con roquefort", "29.00"],
+       ["41- Completo con huevo, con roquefort", "30.00"],
+       ["42- Completo sin huevo, con choclo", "29.00"],
+       ["43- Completo con huevo, con choclo", "30.00"],
+       ["44- Completo sin huevo, con berenjenas", "29.00"],
+       ["45- Completo con huevo, con berenjenas", "30.00"],
+       ["46- Completo sin huevo, con palmitos", "32.00"],
+       ["47- Completo con huevo, con palmitos", "34.00"],
+       ["48- Completo sin huevo, con ajíes o morrones", "31.00"],
+       ["49- Completo con huevo, con ajíes o morrones", "32.00"],
+       ["50- Completo sin huevo, sin pacenta, sin jamón", "23.00"],
+       ["51- Completo con huevo, sin pacenta, sin jamón", "25.00"],
+       ["52- Cebolla, tomate, pepino, lechuga", "24.00"],
+       ["53- Cebolla, tomate, ajo y orégano", "23.00"],
+       ["54- Mayonesa de palta y tomate", "23.00"],
+       ["55- Mayonesa de palta y roquefort", "23.00"],
+       ["56- Solo con roquefort", "22.00"],
+       ["57- Solo con choclo", "22.00"],
+       ["58- Solo con palmitos", "24.00"],
+       ["59- Solo con berenjenas", "23.00"],
+       ["60- Solo con ajíes y morrones", "24.00"],
+       ["61- Solo con pepinos", "24.00"],
+     ]
+   },
+ "PANCHOS" => {
+     "" => [
+       ["62- Solo", "8.00"],
+       ["63- Con queso", "10.00"],
+       ["64- Jamón y queso", "12.00"],
+       ["65- Panceta y cebolla", "11.00"],
+       ["66- Panceta y queso", "12.00"],
+       ["67- Panceta, queso y cebolla", "13.00"],
+       ["68- Con chucrut", "11.00"],
+       ["69- Panceta y chucrut", "12.00"],
+       ["70- Chucrut y queso", "12.00"],
+       ["71- Panceta, queso, cebolla y chucrut", "14.00"],
+     ]
    }
 }.each do |category, subcategories|
   subcategories.each do |subcategory, foods|
